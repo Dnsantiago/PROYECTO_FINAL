b5784b207d615abecbdc9532b7d334d57d67b8e3
DNS <DNS@DNS-PC>
2017-05-25 23:29:00 -0600

    Carga de listas y  lectura de archivos

--- PROYECTO_FINAL/Form1.Designer.cs
+++ PROYECTO_FINAL/Form1.Designer.cs
@@ -29,25 +29,122 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.dataGridViewMostrar1 = new System.Windows.Forms.DataGridView();
+            this.buttonComunidad = new System.Windows.Forms.Button();
+            this.buttonGarage = new System.Windows.Forms.Button();
+            this.buttonGastos = new System.Windows.Forms.Button();
+            this.buttonLocal = new System.Windows.Forms.Button();
+            this.buttonPiso = new System.Windows.Forms.Button();
+            this.buttonPropiedad = new System.Windows.Forms.Button();
+            this.buttonPropietario = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewMostrar1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(176, 112);
+            this.button1.Location = new System.Drawing.Point(12, 54);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(73, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Bancos";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridViewMostrar1
+            // 
+            this.dataGridViewMostrar1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewMostrar1.Location = new System.Drawing.Point(91, 57);
+            this.dataGridViewMostrar1.Name = "dataGridViewMostrar1";
+            this.dataGridViewMostrar1.Size = new System.Drawing.Size(328, 223);
+            this.dataGridViewMostrar1.TabIndex = 1;
+            // 
+            // buttonComunidad
+            // 
+            this.buttonComunidad.Location = new System.Drawing.Point(12, 83);
+            this.buttonComunidad.Name = "buttonComunidad";
+            this.buttonComunidad.Size = new System.Drawing.Size(73, 23);
+            this.buttonComunidad.TabIndex = 2;
+            this.buttonComunidad.Text = "Comunidad";
+            this.buttonComunidad.UseVisualStyleBackColor = true;
+            this.buttonComunidad.Click += new System.EventHandler(this.buttonComunidad_Click);
+            // 
+            // buttonGarage
+            // 
+            this.buttonGarage.Location = new System.Drawing.Point(12, 112);
+            this.buttonGarage.Name = "buttonGarage";
+            this.buttonGarage.Size = new System.Drawing.Size(73, 23);
+            this.buttonGarage.TabIndex = 3;
+            this.buttonGarage.Text = "Garage";
+            this.buttonGarage.UseVisualStyleBackColor = true;
+            this.buttonGarage.Click += new System.EventHandler(this.buttonGarage_Click);
+            // 
+            // buttonGastos
+            // 
+            this.buttonGastos.Location = new System.Drawing.Point(12, 141);
+            this.buttonGastos.Name = "buttonGastos";
+            this.buttonGastos.Size = new System.Drawing.Size(73, 23);
+            this.buttonGastos.TabIndex = 4;
+            this.buttonGastos.Text = "Gastos";
+            this.buttonGastos.UseVisualStyleBackColor = true;
+            this.buttonGastos.Click += new System.EventHandler(this.buttonGastos_Click);
+            // 
+            // buttonLocal
+            // 
+            this.buttonLocal.Location = new System.Drawing.Point(12, 170);
+            this.buttonLocal.Name = "buttonLocal";
+            this.buttonLocal.Size = new System.Drawing.Size(73, 23);
+            this.buttonLocal.TabIndex = 5;
+            this.buttonLocal.Text = "Local";
+            this.buttonLocal.UseVisualStyleBackColor = true;
+            this.buttonLocal.Click += new System.EventHandler(this.buttonLocal_Click);
+            // 
+            // buttonPiso
+            // 
+            this.buttonPiso.Location = new System.Drawing.Point(12, 199);
+            this.buttonPiso.Name = "buttonPiso";
+            this.buttonPiso.Size = new System.Drawing.Size(73, 23);
+            this.buttonPiso.TabIndex = 6;
+            this.buttonPiso.Text = "Pisos";
+            this.buttonPiso.UseVisualStyleBackColor = true;
+            this.buttonPiso.Click += new System.EventHandler(this.buttonPiso_Click);
+            // 
+            // buttonPropiedad
+            // 
+            this.buttonPropiedad.Location = new System.Drawing.Point(12, 228);
+            this.buttonPropiedad.Name = "buttonPropiedad";
+            this.buttonPropiedad.Size = new System.Drawing.Size(73, 23);
+            this.buttonPropiedad.TabIndex = 7;
+            this.buttonPropiedad.Text = "Propiedades";
+            this.buttonPropiedad.UseVisualStyleBackColor = true;
+            this.buttonPropiedad.Click += new System.EventHandler(this.buttonPropiedad_Click);
+            // 
+            // buttonPropietario
+            // 
+            this.buttonPropietario.Location = new System.Drawing.Point(12, 257);
+            this.buttonPropietario.Name = "buttonPropietario";
+            this.buttonPropietario.Size = new System.Drawing.Size(73, 23);
+            this.buttonPropietario.TabIndex = 8;
+            this.buttonPropietario.Text = "Propietario";
+            this.buttonPropietario.UseVisualStyleBackColor = true;
+            this.buttonPropietario.Click += new System.EventHandler(this.buttonPropietario_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(744, 334);
+            this.Controls.Add(this.buttonPropietario);
+            this.Controls.Add(this.buttonPropiedad);
+            this.Controls.Add(this.buttonPiso);
+            this.Controls.Add(this.buttonLocal);
+            this.Controls.Add(this.buttonGastos);
+            this.Controls.Add(this.buttonGarage);
+            this.Controls.Add(this.buttonComunidad);
+            this.Controls.Add(this.dataGridViewMostrar1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewMostrar1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -55,6 +152,14 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridViewMostrar1;
+        private System.Windows.Forms.Button buttonComunidad;
+        private System.Windows.Forms.Button buttonGarage;
+        private System.Windows.Forms.Button buttonGastos;
+        private System.Windows.Forms.Button buttonLocal;
+        private System.Windows.Forms.Button buttonPiso;
+        private System.Windows.Forms.Button buttonPropiedad;
+        private System.Windows.Forms.Button buttonPropietario;
     }
 }
 
--- PROYECTO_FINAL/Form1.cs
+++ PROYECTO_FINAL/Form1.cs
@@ -7,14 +7,187 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace PROYECTO_FINAL
 {
     public partial class Form1 : Form
     {
+
+        // ************************SE DECLARAN TODAS LAS LISTAS *********************
+
+        List<Bancos> listbancos = new List<Bancos>();
+        List<Comunidad> listcomunidad= new List<Comunidad>();
+        List<Garage> listgarage = new List<Garage>();
+        List<Gastos> listgastos = new List<Gastos>();
+        List<Local> listlocal = new List<Local>();
+        List<Piso> listpiso = new List<Piso>();
+        List<Propiedad> listpropiedad = new List<Propiedad>();
+        List<Propietario> listpropietario = new List<Propietario>();
+        
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string direccion = "C:\\users\\DNS\\documents\\PROGRA\\PROGRA2017\\PROYECTO_FINAL\\bancos.txt";
+
+            FileStream stream = new FileStream(direccion, FileMode.Open, FileAccess.Read);
+            StreamReader leer = new StreamReader(stream);
+
+            while (leer.Peek() > -1)
+            {
+                Bancos bancotemp = new Bancos();
+                bancotemp.Id_gastos= leer.ReadLine();
+                bancotemp.Descripcion = leer.ReadLine();
+                bancotemp.Importe = leer.ReadLine();
+                bancotemp.Zona_reparto = leer.ReadLine();
+                listbancos.Add(bancotemp);
+            }
+            leer.Close();
+            dataGridViewMostrar1.DataSource = listbancos;
+            dataGridViewMostrar1.Refresh();
+        }
+
+        private void buttonComunidad_Click(object sender, EventArgs e)
+        {
+            string direccion = "C:\\users\\DNS\\documents\\PROGRA\\PROGRA2017\\PROYECTO_FINAL\\comunidad.txt";
+
+            FileStream stream = new FileStream(direccion, FileMode.Open, FileAccess.Read);
+            StreamReader leer = new StreamReader(stream);
+
+            while (leer.Peek() > -1)
+            {
+                Comunidad comunidadtemp = new Comunidad();
+                comunidadtemp.Identificacion = leer.ReadLine();
+                comunidadtemp.Nombre= leer.ReadLine();
+                comunidadtemp.Poblacion= leer.ReadLine();
+                listcomunidad.Add(comunidadtemp);
+            }
+            leer.Close();
+            dataGridViewMostrar1.DataSource = listcomunidad;
+            dataGridViewMostrar1.Refresh();
+        }
+
+        private void buttonGarage_Click(object sender, EventArgs e)
+        {
+            string direccion = "C:\\users\\DNS\\documents\\PROGRA\\PROGRA2017\\PROYECTO_FINAL\\garage.txt";
+
+            FileStream stream = new FileStream(direccion, FileMode.Open, FileAccess.Read);
+            StreamReader leer = new StreamReader(stream);
+
+            while (leer.Peek() > -1)
+            {
+                Garage garagetemp = new Garage();
+                garagetemp.Abierta = leer.ReadLine();
+                garagetemp.Bodega = leer.ReadLine();
+                listgarage.Add(garagetemp);
+            }
+            leer.Close();
+            dataGridViewMostrar1.DataSource = listgarage;
+            dataGridViewMostrar1.Refresh();
+        }
+
+        private void buttonGastos_Click(object sender, EventArgs e)
+        {
+            string direccion = "C:\\users\\DNS\\documents\\PROGRA\\PROGRA2017\\PROYECTO_FINAL\\gastos.txt";
+
+            FileStream stream = new FileStream(direccion, FileMode.Open, FileAccess.Read);
+            StreamReader leer = new StreamReader(stream);
+
+            while (leer.Peek() > -1)
+            {
+                Gastos gastostemp = new Gastos();
+                gastostemp.Identificaion = leer.ReadLine();
+                gastostemp.Nombre = leer.ReadLine();
+                gastostemp.Tipo_reparto = leer.ReadLine();
+                listgastos.Add(gastostemp);
+            }
+            leer.Close();
+            dataGridViewMostrar1.DataSource = listgastos;
+            dataGridViewMostrar1.Refresh();
+        }
+
+        private void buttonLocal_Click(object sender, EventArgs e)
+        {
+            string direccion = "C:\\users\\DNS\\documents\\PROGRA\\PROGRA2017\\PROYECTO_FINAL\\local.txt";
+
+            FileStream stream = new FileStream(direccion, FileMode.Open, FileAccess.Read);
+            StreamReader leer = new StreamReader(stream);
+
+            while (leer.Peek() > -1)
+            {
+                Local localtemp = new Local();
+                localtemp.Nombre_comercial = leer.ReadLine();
+                localtemp.Acitvidad = leer.ReadLine();
+               listlocal.Add(localtemp);
+            }
+            leer.Close();
+            dataGridViewMostrar1.DataSource = listlocal;
+            dataGridViewMostrar1.Refresh();
+        }
+
+        private void buttonPiso_Click(object sender, EventArgs e)
+        {
+            string direccion = "C:\\users\\DNS\\documents\\PROGRA\\PROGRA2017\\PROYECTO_FINAL\\piso.txt";
+
+            FileStream stream = new FileStream(direccion, FileMode.Open, FileAccess.Read);
+            StreamReader leer = new StreamReader(stream);
+
+            while (leer.Peek() > -1)
+            {
+                Piso pisotemp = new Piso();
+                pisotemp.Vhvn = leer.ReadLine();
+                pisotemp.Cantidad_habitaciones = Convert.ToInt32( leer.ReadLine());
+               listpiso.Add(pisotemp);
+            }
+            leer.Close();
+            dataGridViewMostrar1.DataSource = listpiso;
+            dataGridViewMostrar1.Refresh();
+        }
+
+        private void buttonPropiedad_Click(object sender, EventArgs e)
+        {
+            string direccion = "C:\\users\\DNS\\documents\\PROGRA\\PROGRA2017\\PROYECTO_FINAL\\propiedad.txt";
+
+            FileStream stream = new FileStream(direccion, FileMode.Open, FileAccess.Read);
+            StreamReader leer = new StreamReader(stream);
+
+            while (leer.Peek() > -1)
+            {
+                Propiedad propiedadtemp = new Propiedad();
+                propiedadtemp.Codigo_propiedad = leer.ReadLine();
+                propiedadtemp.Tipo = leer.ReadLine();
+                propiedadtemp.Metros_cuadrados = Convert.ToInt32( leer.ReadLine());
+                propiedadtemp.Codigo_propietario = leer.ReadLine();
+                propiedadtemp.Lista_gastos = leer.ReadLine();
+                listpropiedad.Add(propiedadtemp);
+            }
+            leer.Close();
+            dataGridViewMostrar1.DataSource = listpropiedad;
+            dataGridViewMostrar1.Refresh();
+        }
+
+        private void buttonPropietario_Click(object sender, EventArgs e)
+        {
+            string direccion = "C:\\users\\DNS\\documents\\PROGRA\\PROGRA2017\\PROYECTO_FINAL\\propietario.txt";
+
+            FileStream stream = new FileStream(direccion, FileMode.Open, FileAccess.Read);
+            StreamReader leer = new StreamReader(stream);
+
+            while (leer.Peek() > -1)
+            {
+                Propietario propietariotemp = new Propietario();
+                propietariotemp.Nit = leer.ReadLine();
+                propietariotemp.Nombre = leer.ReadLine();
+                propietariotemp.Email= leer.ReadLine();
+                listpropietario.Add(propietariotemp);
+            }
+            leer.Close();
+            dataGridViewMostrar1.DataSource =listpropietario;
+            dataGridViewMostrar1.Refresh();
         }
     }
 }
